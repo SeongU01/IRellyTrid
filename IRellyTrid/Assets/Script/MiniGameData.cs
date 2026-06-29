f1f5138a9e981a793ac4d19e14c3947081249504
SeongU <blacknaco85@gmail.com>
2026-06-29 18:06:32 +0900

    feat : minigame data 추가

---
+++ IRellyTrid/Assets/Script/MiniGameData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class MiniGameData
+{
+    public string name;
+    public string commandText;
+    public float timeLimit = 30f;
+    public MiniGameBase prefeb;
+}
