a0843016121244e56519585136eb8680915230b6
SeongU <blacknaco85@gmail.com>
2026-06-29 18:07:16 +0900

    feat : minigame base 코드 추가
    앞으로 각 미니게임은 이 class를 상속받아 구현 하면됨.

---
+++ IRellyTrid/Assets/Script/MiniGameBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class MiniGameBase : MonoBehaviour
+{
+    public event Action<MiniGameResult> OnFinished;
+
+    private bool isPlaying;
+
+    // getter
+    public bool IsPlaying => isPlaying;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    public virtual void Init()
+    {
+        isPlaying = true;
+    }
+    
+    public void Play()
+    {
+        if (isPlaying)
+            return;
+
+        isPlaying = true;
+        OnStart();
+    }
+
+    public void Stop()
+    {
+        if (!isPlaying)
+            return;
+
+        isPlaying = false;
+        OnEnd();
+    }
+
+    protected void Success()
+    {
+        Finish(true);
+    }
+    private void Finish(bool success)
+    {
+        if (!isPlaying)
+            return;
+
+        isPlaying = false;
+        OnFinished?.Invoke(new MiniGameResult(success));
+    }
+    protected abstract void OnStart();
+    protected virtual void OnEnd()
+    {
+    }
+}
