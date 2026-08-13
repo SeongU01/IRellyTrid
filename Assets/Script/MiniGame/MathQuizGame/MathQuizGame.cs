e50a68d81a0defb6cc92ffe4bd103c1ac8a0f391
SeongU <blacknaco85@gmail.com>
2026-08-14 00:31:47 +0900

    fix: 수학 미니게임 정답 위치 보정

--- Assets/Script/MiniGame/MathQuizGame/MathQuizGame.cs
+++ Assets/Script/MiniGame/MathQuizGame/MathQuizGame.cs
@@ -41,11 +41,11 @@ public class MathQuizGame : MiniGameBase
     [SerializeField] private Sprite firstVariantNoteSprite;
     [SerializeField] private Sprite secondVariantNoteSprite;
     [SerializeField] private Vector2 baseAnswerPosition =
-        new Vector2(-263.05054f, -274.1f);
+        new Vector2(-272.58f, -279.58f);
     [SerializeField] private Vector2 firstVariantAnswerPosition =
-        new Vector2(-247.45053f, -235.1f);
+        new Vector2(-262.21f, -260.06f);
     [SerializeField] private Vector2 secondVariantAnswerPosition =
-        new Vector2(-235.75053f, -239f);
+        new Vector2(-261.74f, -260.78f);
     [SerializeField] private TMP_Text questionText;
     [SerializeField] private TMP_Text progressText;
     [FormerlySerializedAs("inputText")]
